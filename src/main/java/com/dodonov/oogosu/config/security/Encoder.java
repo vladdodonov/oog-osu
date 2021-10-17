fe8f10efd7266159d979d0b5c98781ea50468d23
Владислав Додонов <dodonovvy@it2g.ru>
2021-10-17 17:22:47 +0500

    1

---
+++ src/main/java/com/dodonov/oogosu/config/security/Encoder.java
@@ -0,0 +1,13 @@
+package com.dodonov.oogosu.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class Encoder {
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/dodonov/oogosu/config/security/SecurityConfig.java
+++ src/main/java/com/dodonov/oogosu/config/security/SecurityConfig.java
@@ -17,73 +17,71 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.servlet.http.HttpServletResponse;
+
 @EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
 @EnableWebSecurity// Enable security config. This annotation denotes config for spring security.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	private final UserDetailsService userDetailsService;
-	private final JwtConfig jwtConfig;
+    private final UserDetailsService userDetailsService;
+    private final JwtConfig jwtConfig;
+    private final BCryptPasswordEncoder encoder;
 
-	public SecurityConfig(@Lazy UserDetailsService userDetailsService, JwtConfig jwtConfig) {
-		this.userDetailsService = userDetailsService;
-		this.jwtConfig = jwtConfig;
-	}
+    public SecurityConfig(@Lazy UserDetailsService userDetailsService, JwtConfig jwtConfig, BCryptPasswordEncoder encoder) {
+        this.userDetailsService = userDetailsService;
+        this.jwtConfig = jwtConfig;
+        this.encoder = encoder;
+    }
 
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-		    .csrf().disable()
-		     // make sure we use stateless session; session won't be used to store user's state.
-	            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-	        .and()
-	         // handle an authorized attempts
-	            .exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
-	        .and()
-		    // Add a filter to validate user credentials and add token in the response header
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                // make sure we use stateless session; session won't be used to store user's state.
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                // handle an authorized attempts
+                .exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                .and()
+                // Add a filter to validate user credentials and add token in the response header
 
-		    // What's the authenticationManager()?
-		    // An object provided by WebSecurityConfigurerAdapter, used to authenticate the user passing user's credentials
-		    // The filter needs this auth manager to authenticate the user.
-		    .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfig))
-				.addFilterBefore(new JwtTokenAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class)
-				.authorizeRequests()
-				// allow all who are accessing "auth" service
-				.antMatchers(jwtConfig.getUri()).permitAll()
-				.antMatchers("/api/appeal/create/**").permitAll()
-				// must be an admin if trying to access admin area (authentication is also required here)
-				//.antMatchers("/department/**").hasRole("ADMIN")
-				// Any other request must be authenticated
-				.anyRequest().authenticated();
-	}
+                // What's the authenticationManager()?
+                // An object provided by WebSecurityConfigurerAdapter, used to authenticate the user passing user's credentials
+                // The filter needs this auth manager to authenticate the user.
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfig))
+                .addFilterBefore(new JwtTokenAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                // allow all who are accessing "auth" service
+                .antMatchers(jwtConfig.getUri()).permitAll()
+                .antMatchers("/api/appeal/create/**").permitAll()
+                // must be an admin if trying to access admin area (authentication is also required here)
+                //.antMatchers("/department/**").hasRole("ADMIN")
+                // Any other request must be authenticated
+                .anyRequest().authenticated();
+    }
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-	}
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+    }
 
-	@Bean
-	public JwtConfig jwtConfig() {
-		return new JwtConfig();
-	}
+    @Bean
+    public JwtConfig jwtConfig() {
+        return new JwtConfig();
+    }
 
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
 
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-		return source;
-	}
-
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web
-				.ignoring()
-				.antMatchers("/v2/api-docs", "/swagger-resources",
-						"/swagger-resources/configuration/security", "/swagger-ui.html",
-						"/webjars/**", "/swagger-resources/configuration/ui");
-	}
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/v2/api-docs", "/swagger-resources",
+                        "/swagger-resources/configuration/security", "/swagger-ui.html",
+                        "/webjars/**", "/swagger-resources/configuration/ui");
+    }
 }
